de38c8c8919056f154b55c6e1813710f459ac77f
Adrián Vicente Vera <adrianvicentevera04@gmail.com>
2026-09-10 19:41:01 +0200

    Modificación diseño frontend.

--- frontend/src/app/components/home/home/home.component.ts
+++ frontend/src/app/components/home/home/home.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  // Inyección de dependencias
+
+  // Declaración de variables
 
 }
