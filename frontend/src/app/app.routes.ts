222ff01e5c73dc7c52f7c44350bbdee5736faad7
Adrián Vicente Vera <adrianvicentevera04@gmail.com>
2026-09-10 19:39:08 +0200

    Nuevas rutas definidias en app.routes.ts para los componentes recién creados.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,34 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home/home.component';
+import { ColeccionDetailComponent } from './components/colecciones/coleccion-detail/coleccion-detail.component';
+import { ColeccionFormComponent } from './components/colecciones/coleccion-form/coleccion-form.component';
+import { ColeccionListComponent } from './components/colecciones/coleccion-list/coleccion-list.component';
+import { ProductoDetailComponent } from './components/productos/producto-detail/producto-detail.component';
+import { ProductoFormComponent } from './components/productos/producto-form/producto-form.component';
+import { ProductoListComponent } from './components/productos/producto-list/producto-list.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 
 export const routes: Routes = [
 
   // Redirección a página principal
 
   { path: '', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+
+  // Declaración de rutas para componente productos
+
+  { path: 'coleccion-detalle', component: ColeccionDetailComponent},
+  { path: 'coleccion-formulario', component: ColeccionFormComponent},
+  { path: 'coleccion-lista', component: ColeccionListComponent},
+
+  // Declaración de rutas para componente colecciones
+
+  { path: 'producto-detalle', component: ProductoDetailComponent},
+  { path: 'producto-formulario', component: ProductoFormComponent},
+  { path: 'producto-lista', component: ProductoListComponent},
+
+  // Declaración de rutas para componente usuario
+
+  { path: 'admin-dashboard', component: DashboardComponent}
 
 ];
