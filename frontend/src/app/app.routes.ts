a93f8d7861608821bc51b430e3b7d48b04f88093
Adrián Vicente Vera <adrianvicentevera04@gmail.com>
2026-09-10 19:24:26 +0200

    Modificación en la estructura del frontend.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home/home.component';
 
 export const routes: Routes = [
 
   // Redirección a página principal
+
+  { path: '', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent }
 
 ];
