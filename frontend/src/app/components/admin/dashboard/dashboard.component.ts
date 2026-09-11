a03f8591c295b01bde9d676ae3b425425e0e1b27
Adrián Vicente Vera <adrianvicentevera04@gmail.com>
2026-09-11 12:39:01 +0200

    Modificación de diseño en el panel de administrador de la plataforma.

--- frontend/src/app/components/admin/dashboard/dashboard.component.ts
+++ frontend/src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { AdminHeaderComponent } from "../../dise\u00F1o/admin-header/admin-header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [AdminHeaderComponent, CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
+  standalone: true
 })
 export class DashboardComponent {
 
